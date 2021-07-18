64956874fac4f61fdcc9a12504ef4fab158647ec
Jerome Onwunalu <jeronwunalu@gmail.com>
2021-07-18 17:51:29 -0500

    Modify layer-sizes to accept a default hidden layer size

--- src/utilities/utilities.lisp
+++ src/utilities/utilities.lisp
@@ -12,7 +12,7 @@
   "Returns the number of rows in matrix X."
   (nth 1 (array-dimensions X)))
 
-(defun layer-sizes (X, Y)
+(defun layer-sizes (X, Y, &optional (n-h 4))
   "Arguments:
    X -- input dataset of shape (input size, number of examples)
    Y -- labels of shape (output size, number of examples
@@ -21,4 +21,7 @@
    n-x -- the size of the input layer
    n-h -- the size of the hidden layer
    n-y -- the size of the output layer"
-  (values (num-rows X) (num-cols X) (num-rows)))
+  (let ((n-x (num-rows X))
+        (n-y (num-rows Y)))
+    (assert (= (num-cols X) (= num-cols Y) nil "Different number of samples in X and Y.")))
+  (values n-x n-h n-y))
