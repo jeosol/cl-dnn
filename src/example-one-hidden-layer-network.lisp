54d83c230ccaee17e7662cd3586f8517d5e7a5ed
Jerome Onwunalu <jeronwunalu@gmail.com>
2021-07-21 18:18:21 -0500

    Add example skeleton code for one hidden layer mnist example

---
+++ src/example-one-hidden-layer-network.lisp
@@ -0,0 +1,8 @@
+(uiop/package:define-package :src/example-one-hidden-layer-network
+    (:use :cl)
+  (:use-reexport :src/architecture/one-hidden-layer-network))
+
+(in-package :src/example-one-hidden-layer-network)
+
+(defun example-one-hidden-layer-mnist-classifiier ()
+  )
