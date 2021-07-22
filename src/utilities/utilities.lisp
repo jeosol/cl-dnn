8a2c9374ead00298492903c69d9699a333a8bf61
Jerome Onwunalu <jeronwunalu@gmail.com>
2021-07-22 11:53:22 -0500

    Delete exported function which has been moved

--- src/utilities/utilities.lisp
+++ src/utilities/utilities.lisp
@@ -1,7 +1,6 @@
 (uiop/package:define-package :src/utilities/utilities
     (:use :cl)
-  (:nicknames :cl-dnn-utilities)
-  (:export #:layer-sizes))
+  (:nicknames :cl-dnn-utilities))
 
 (in-package :src/utilities/utilities)
 
