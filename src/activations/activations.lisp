9e1b4bc4fb2539d7adc77021ab4e1971df38eba2
Jerome Onwunalu <jeronwunalu@gmail.com>
2021-07-27 23:17:59 -0500

    Fix error related to matrix dimensions. Add softmax activation

--- src/activations/activations.lisp
+++ src/activations/activations.lisp
@@ -3,7 +3,8 @@
   (:use-reexport :src/array/array)
   (:export #:tanh-activation
            #:relu-activation
-           #:sigmoid-activation))
+           #:sigmoid-activation
+           #:softmax-activation))
 
 (in-package :src/activations/activations)
 
@@ -12,8 +13,8 @@
   (let* ((rows (num-rows matrix))
          (cols (num-cols matrix))
          (new-matrix (make-matrix rows cols)))
-    (dotimes (i rows)
-      (dotimes (j cols)
+    (dotimes (j cols)
+      (dotimes (i rows)
         (setf (aref (aref new-matrix i) j) (/ (- (exp (aref (aref matrix i) j))
                                                  (exp (- (aref (aref matrix i) j))))
                                               (+ (exp (aref (aref matrix i) j))
@@ -25,8 +26,8 @@
   (let* ((rows (num-rows matrix))
          (cols (num-cols matrix))
          (new-matrix (make-matrix rows cols)))
-    (dotimes (i rows)
-      (dotimes (j cols)
+    (dotimes (j cols)
+      (dotimes (i rows)
         (setf (aref (aref new-matrix i) j) (if (< (aref (aref matrix i) j) 0.0) 0.0 (aref (aref matrix i) j)))))
     new-matrix))
 
@@ -38,4 +39,18 @@
     (dotimes (i rows)
       (dotimes (j cols)
         (setf (aref (aref new-matrix i) j) (/ 1 (+ 1 (exp (- (aref (aref matrix i) j))))))))
+    new-matrix))
+
+(defun softmax-activation (matrix)
+  "Implements the softmax activation function"
+  (let* ((rows (num-rows matrix))
+         (cols (num-cols matrix))
+         (new-matrix (make-matrix rows cols))
+         (sum 0.0))
+    (dotimes (j cols)
+      (setf sum 0.0)
+      (dotimes (i rows)
+        (incf sum (exp (aref (aref matrix i) j))))
+      (dotimes (i rows)
+        (setf (aref (aref new-matrix i) j) (/ (exp (aref (aref matrix i) j)) sum))))
     new-matrix))
