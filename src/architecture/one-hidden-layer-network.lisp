ed023631ebb02f8e2fa9142a7f54ab36ebe1bb6d
Jerome Onwunalu <jeronwunalu@gmail.com>
2021-07-27 23:19:53 -0500

    Add several updates and fix bugs.
    This commits corrects the equation for the cross entropy used to compute the
    cost. A function for computing accuracy was also added.
    
    This commit also fixes that results because of the way the data is
    represented. The code runs now but it's very slow. We will plan to optimize it
    later on

--- src/architecture/one-hidden-layer-network.lisp
+++ src/architecture/one-hidden-layer-network.lisp
@@ -19,9 +19,9 @@
    n-x -- the size of the input layer
    n-h -- the size of the hidden layer
    n-y -- the size of the output layer"
-  (let ((n-x (num-rows x))
-        (n-y (num-rows y)))
-    (assert (= n-x n-y) nil "Different number of samples in X and Y.")
+  (let ((n-x (num-cols x)) ;; x is of shape (m n-x)
+        (n-y (length (aref y 0))))
+    ;(assert (= (num-rows x) n-y) nil "Different number of samples in X and Y.")
     (values n-x n-h n-y)))
 
 ;;; TODO:
@@ -55,7 +55,7 @@
 ;;; FORWARD PROPAGATION
 (defun forward-propagation-one-hidden-layer (x parameters
                                              &optional (activation-functions '(relu-activation
-                                                                               sigmoid-activation)))
+                                                                               softmax-activation)))
   "Perform forward propagation.
 
   Argument:
@@ -89,14 +89,19 @@
 
    Returns:
    Cost -- cross-entropy"
-  (let* ((m (length y))
-         (num-outputs (length (aref y 0)))
+  (format t "~&Computing cost: dimensions a2=~d, dimensions y=~d" (dimensions a2) (dimensions y))
+  (let* ((m (num-cols y))
+         (num-outputs (num-rows y))
          (sum 0.0))
+    ;; (dotimes (i m)
+    ;;   (dotimes (k num-outputs)
+    ;;     ;(print (list i k m num-outputs (num-cols a2) (aref (aref y k) i) (aref (aref a2 k) i)))
+    ;;     (incf sum (+ (* (aref (aref y k) i) (log (aref (aref a2 k) i)))
+    ;;                  (* (- 1.0 (aref (aref y k) i)) (log (- 1.0 (aref (aref a2 k) i))))))))
     (dotimes (i m)
       (dotimes (k num-outputs)
-        (incf sum (+ (* (aref (aref y i) k) (log (aref (aref a2 i) k)))
-                     (* (- 1.0 (aref (aref y i) k)) (log (- 1.0 (aref (aref a2 i) k))))))))
-    (* (/ 1.0 m) sum)))
+        (incf sum (+ (* (aref (aref y k) i) (log (aref (aref a2 k) i)))))))
+    (* (- (/ 1.0 m) sum))))
 
 (defun backward-propagation-one-hidden-layer (parameters cache x y)
   "Perform backward propagation.
@@ -114,9 +119,9 @@
          ;;(w1 (gethash "w1" parameters))
          (w2 (gethash "w2" parameters))
          (a1 (gethash "a1" cache))
-         (a2 (gethash "a2" cache))
+         (a2 (gethash "a2" cache)) ;(dummy (print (list (print 'here) (dimensions a2) (dimensions y))))
          ;; backward propagation to calculate dw1, dw2, db1, db2, dz1, dz2
-         (dz2 (matrix-matrix-subtract a2 y))
+         (dz2 (matrix-matrix-subtract a2 y)) ;(dummy (print 'here2))
          ;(dummy (print (list "dim sz2" (dimensions dz2) "dim a1" (dimensions a1))))
          (dw2 (matrix-scalar-multiply (matrix-matrix-multiply dz2 (transpose-matrix a1)) (/ 1.0 m)))
 
@@ -203,11 +208,16 @@
   (assert (= (num-cols predicted-labels) (num-cols actual-labels))
           nil "The length of predicted and actual label data are not equal")
   (let* ((m (num-cols predicted-labels))
+         (num-outputs (num-rows predicted-labels))
          (correct 0)
          (pred-label) (true-label))
     (dotimes (i m)
-      (setf pred-label (argmax (aref predicted-labels i))
-            true-label (argmax (aref actual-labels i)))
+      (setf pred-label (argmax (map 'vector #'identity
+                                    (loop :for k :from 0 :below num-outputs
+                                          :collect (aref (aref predicted-labels k) i))))
+            true-label (argmax (map 'vector #'identity
+                                    (loop :for k :from 0 :below num-outputs
+                                          :collect (aref (aref actual-labels k) i)))))
       ;(print (list "accuracy-info = " i pred-label true-label))
       (when (= pred-label true-label)
         (incf correct)))
@@ -216,7 +226,7 @@
 
 ;;; Neural network model with one hidden layer
 (defun nn-model-one-hidden-layer (train-x train-y n-h &key
-                                                        (batch-size 32) (num-epochs 10000)
+                                                        (batch-size 64) (num-epochs 100)
                                                         valid-x valid-y
                                                         (print-output-p nil))
   "Run the one-hidden layer neural newtwork model.
@@ -228,16 +238,22 @@
   print-cost     -- if T, print the cost every 1000 iterations"
   (let* (dummy n-x n-y parameters a2 cache cost grads cost-history
          batch-indices (mini-batch-counter 0) (epoch 1)
+         (num-batches 0)
+         (num-data (num-rows train-x))
          (iter-counter 0)
          shuffle-indices
-         (learning-rate 1.2) (decay-rate 1.0)
-         (validation-accuracy nil))
+         (learning-rate)
+         (learning-rate-init 0.199) (decay-rate 0.999) (decay-steps 10)
+         (validation-accuracy nil)
+         (valid-x (transpose-matrix valid-x))
+         (valid-y (transpose-matrix valid-y)))
     ;; Get dimensions for the input and output layers
     (setf (values n-x dummy n-y) (layer-sizes train-x train-y n-h))
     ;; Get shuffled indices
-    (setf shuffle-indices (map 'vector #'identity (loop :for i :from 0 :below n-x :collect i)))
+    (setf shuffle-indices (map 'vector #'identity (loop :for i :from 0 :below num-data :collect i)))
     ;; Determine the batch start and end indices
-    (setf batch-indices (get-batch-start-end-indices n-x batch-size))
+    (setf (values batch-indices num-batches) (get-batch-start-end-indices num-data batch-size))
+    (format t "~&Number of batches = ~d" (length batch-indices))
     ;; Initialize parameters
     (setf parameters (initialize-parameters-one-hidden-layer n-x n-h n-y))
     ;; Loop over the training data
@@ -246,9 +262,13 @@
       (setf shuffle-indices (alexandria:shuffle shuffle-indices))
       ;; Do batch gradient descent
       (loop :for (start-index end-index) :in batch-indices
-            :for batch-x = (slice-data train-x start-index end-index shuffle-indices)
-            :for batch-y = (slice-data train-y start-index end-index shuffle-indices)
+            :for batch-counter :from 1
+            :for batch-x = (transpose-matrix (slice-data train-x start-index end-index shuffle-indices))
+            :for batch-y = (transpose-matrix (slice-data train-y start-index end-index shuffle-indices))
             :do
+               ;(print (length batch-x))
+               (format t "~&Batch information: start=~6d, end=~6d: dimensions: batch-x=~s, batch-y=~s"
+                       start-index end-index (num-cols batch-x) (num-cols batch-y))
                ;; increment iteration counter
                (incf iter-counter)
                ;; Forward propagation step
@@ -259,7 +279,8 @@
                ;; Backward propagation step
                (setf grads (backward-propagation-one-hidden-layer parameters cache batch-x batch-y))
                ;; reduce the learning parameter
-               (setf learning-rate (* learning-rate (/ 1.0 (+ 1.0 (* decay-rate iter-counter)))))
+               ;;(setf learning-rate (* learning-rate-init (/ 1.0 (+ 1.0 (* decay-rate iter-counter)))))
+               (setf learning-rate (* learning-rate-init (expt decay-rate (/ iter-counter decay-steps))))
                ;; Update parameters
                (setf parameters (update-parameters-one-hidden-layer parameters grads learning-rate))
                ;; compute accuracy of the validation data using the current network parameters
@@ -268,10 +289,10 @@
                    (setf predictions (forward-propagation-one-hidden-layer valid-x parameters))
                    (setf validation-accuracy (compute-accuracy predictions valid-y))))
                (when print-output-p
-                 (format t "Epoch ~5d of ~5d: Cost after iterations ~5d: ~18,12f~&~a" k num-epochs
-                         iter-counter cost
+                 (format t "~&Epoch ~5d of ~5d: Iteration = ~5d, Batch ~5d of ~5d, LR=~7,4f, Cost=~18,12f~a~&"
+                         k num-epochs iter-counter batch-counter num-batches learning-rate cost
                          (if validation-accuracy
-                             (format nil "accuracy = ~6,3f" validation-accuracy) "")))))
+                             (format nil ", Accuracy = ~6,4f" validation-accuracy) "")))))
     ;; Return final parameters
     ;(values parameters (reverse cost-history))
     ))
